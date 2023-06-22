0d4d4bb33c1e360aa59b5273915c2a9faca1db2b
NorthSoar <squintyhuey@icloud.com>
2023-06-22 00:46:31 -0700

    Update CSC_110_QT6.py

--- Quick Test 6/CSC_110_QT6.py
+++ Quick Test 6/CSC_110_QT6.py
@@ -1,4 +1,4 @@
 name = "Retracted name" # declare variable name and assign it to name to make program modular
 
 for i in range(27): #create a for loop to run the code 27 times
-    print("Welcome to third week of CSC 110 class at South Seattle College, " + name + "!") # print out the statement and the name assigned to the variable
+    print("Welcome to third week of CSC 110 class at [Retracted School], " + name + "!") # print out the statement and the name assigned to the variable
